86479410219aeff00ecf83effe435383b24e33d9
reydon227 <dhruvarora@delhinerds.com>
2019-02-03 20:01:06 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { AppProvider } from '../providers/app/app';
 import { Push, PushObject, PushOptions } from '@ionic-native/push';
+import { NewsProvider } from '../providers/news/news';
+import { Network } from "@ionic-native/network";
 
 @Component({
   templateUrl: `app.html`
@@ -12,6 +14,9 @@ import { Push, PushObject, PushOptions } from '@ionic-native/push';
 export class MyApp {
   rootPage: any = HomePage;
   pages: Array<{title: string, id: any}>;
+  private news:NewsProvider;
+  private network: Network;
+  public isInternet: boolean;
 
   constructor(platform: Platform, public app: AppProvider, statusBar: StatusBar,
     splashScreen: SplashScreen, private push: Push, public alertCtrl: AlertController) {
@@ -21,11 +26,24 @@ export class MyApp {
       splashScreen.hide();
       this.initPush();
     });
+    
 
     this.pages = [
       {title: "Home", id:"1"}
     ]
 
+  }
+
+  loadCategoryList() {
+    if (this.network.type !== "none") {
+      this.news.getCategories().subscribe(data => {
+        this.app.category = this.news.formatResponse(data);
+        console.log(this.app.category);
+        this.changeTab(this.app.category[0].id);
+      });
+    } else {
+      this.isInternet = true;
+    }
   }
 
   initPush() {
